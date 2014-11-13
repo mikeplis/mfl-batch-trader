1c4fec037f661ae0fd0516e9ab7897305b84a2f1
Mike Plis <mplis@novus.com>
2014-11-13 12:13:06 -0500

    correctly retrieving session id

--- batch_trader.py
+++ batch_trader.py
@@ -1,7 +1,7 @@
 import sys
 import urllib
 import urllib2
-from lxml import etree
+import xml.etree.ElementTree as ET
 
 test_league_id = 67486
 franchise_id = sys.argv[1]
@@ -11,7 +11,7 @@ params = urllib.urlencode({'L': test_league_id, 'FRANCHISE_ID': franchise_id, 'P
 url = "http://football21.myfantasyleague.com/2014/login?%s" % params
 
 f = urllib2.urlopen(url)
-data = etree.fromstring(f.read())
+data = ET.fromstring(f.read())
 
-print(data)
+print(data.attrib)
 
