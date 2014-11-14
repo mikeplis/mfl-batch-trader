10fe5fdf98bae6ff633a88f9235ef5b50cdee5f0
Mike Plis <mplis@novus.com>
2014-11-14 14:35:24 -0500

    added TODO list for next steps

--- batch_trader.py
+++ batch_trader.py
@@ -23,6 +23,20 @@ params = urllib.urlencode({'OFFEREDTO': '0002', 'WILL_GIVE_UP': 9823, 'WILL_RECE
 url = 'http://football19.myfantasyleague.com/2014/import?%s' % params
 req = opener.open(url)
 
+"""
+TODO
+* Offer player to every other owner
+** Retrieve list of other owners from MFL
+** Extract all owners that aren't user
+** Retrieve list of players for each owner and pick one at random
+** Create trade offer
+* Offer player to every other owner in exchange for a draft pick
+** Retrieve list of draft picks for each owner and pick one at random
+** Create trade offer
+* Offer player to every other owner in exchange for specific draft pick 
+** Take year and round as input
+** If owner has draft pick in that year/round, create trade offer. Else, skip
+"""
 
 
 
