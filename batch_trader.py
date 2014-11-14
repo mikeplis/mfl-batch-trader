5a780687f53de4034a633fdedd4e359e4f590858
Mike Plis <mplis@novus.com>
2014-11-14 13:37:07 -0500

    successfully creating trade request

--- batch_trader.py
+++ batch_trader.py
@@ -3,15 +3,27 @@ import urllib
 import urllib2
 import xml.etree.ElementTree as ET
 
-test_league_id = 67486
-franchise_id = sys.argv[1]
-password = sys.argv[2]
+test_league_id = '67486'
+franchise_id = '0001'
+password = sys.argv[1]
 
 params = urllib.urlencode({'L': test_league_id, 'FRANCHISE_ID': franchise_id, 'PASSWORD': password, 'XML': 1})
-url = "http://football21.myfantasyleague.com/2014/login?%s" % params
+url = "http://football19.myfantasyleague.com/2014/login?%s" % params
 
 f = urllib2.urlopen(url)
 data = ET.fromstring(f.read())
 
-print(data.attrib)
+user_id = data.attrib['session_id']
+print(user_id)
+
+opener = urllib2.build_opener()
+opener.addheaders.append(('Cookie', 'USER_ID=%s' % user_id))
+params = urllib.urlencode({'OFFEREDTO': '0002', 'WILL_GIVE_UP': 9823, 'WILL_RECEIVE': 10261, 'TYPE': 'tradeProposal', 'L': test_league_id})
+
+url = 'http://football19.myfantasyleague.com/2014/import?%s' % params
+req = opener.open(url)
+
+
+
+
 
