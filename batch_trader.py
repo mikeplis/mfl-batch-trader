d628267cc5d0983fd224c5442a1546a9a22ce8ea
mplis <michael.plis01@gmail.com>
2014-11-17 08:41:35 -0500

    sends offer of player for draft pick to all other owners; draft_pick year/round are hard-coded; offer is sent regardless of whether owner owns pick in that year/round

--- batch_trader.py
+++ batch_trader.py
@@ -6,6 +6,7 @@ import json
 
 test_league_id = '67486'
 franchise_id = '0001'
+will_give_up_id = '9823'
 password = sys.argv[1]
 
 params = urllib.urlencode({'L': test_league_id, 'FRANCHISE_ID': franchise_id, 'PASSWORD': password, 'XML': 1})
@@ -19,21 +20,22 @@ user_id = data.attrib['session_id']
 opener = urllib2.build_opener()
 opener.addheaders.append(('Cookie', 'USER_ID=%s' % user_id))
 
-rosters_req = 'http://football21.myfantasyleague.com/2014/export?TYPE=rosters&L=%s&W=&JSON=1' % test_league_id
-rosters_resp = urllib2.urlopen(rosters_req)
-rosters = json.loads(rosters_resp.read())
-roster_list = rosters['rosters']['franchise']
+pick_year = '2015'
+pick_round = '1'
 
-for roster in roster_list:
-    roster_id = roster['id']
-    if roster_id != franchise_id:
-        # TODO: check if roster['player'] is list; if roster size == 1, roster['player'] is single object
-        will_receive_id = roster['player'][0]['id']
-        params = urllib.urlencode({'OFFEREDTO': roster_id, 'WILL_GIVE_UP': 9823, 'WILL_RECEIVE': will_receive_id, 'TYPE': 'tradeProposal', 'L': test_league_id})
+draft_picks_req = 'http://football21.myfantasyleague.com/2014/export?TYPE=futureDraftPicks&L=%s&W=&JSON=1' % test_league_id
+draft_picks_resp = urllib2.urlopen(draft_picks_req)
+dp = json.loads(draft_picks_resp.read())
+draft_picks = dp['futureDraftPicks']['franchise']
+
+for draft_pick in draft_picks:
+    fid = draft_pick['id']
+    if fid != franchise_id:
+        will_receive_id = 'FP_{0}_{1}_{2}'.format(fid, pick_year, pick_round)
+        params = urllib.urlencode({'OFFEREDTO': fid, 'WILL_GIVE_UP': will_give_up_id, 'WILL_RECEIVE': will_receive_id, 'TYPE': 'tradeProposal', 'L': test_league_id})
         url = 'http://football19.myfantasyleague.com/2014/import?%s' % params
         print(url)
-        #opener.open(url)
-
+        opener.open(url)
 
 # WILL_GIVE_UP = player that user will give up; player that other owner will receive
 # WILL_RECEIVE = player that user will receive; player that other owner will give up
