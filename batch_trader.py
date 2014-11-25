2bed80800c1d0d62d1a917ea45486138afedeceb
Mike Plis <mplis@novus.com>
2014-11-25 17:32:22 -0500

    refactored batch function to take in list of dicts representing an offer; added documentation for batch function

--- batch_trader.py
+++ batch_trader.py
@@ -31,40 +31,83 @@ class Trader:
         self.franchise_id = franchise_id
         self.opener = opener
 
-    def batch(self, will_give_up, pick_year, pick_round, dry_run=False):
-        if isinstance(will_give_up, list):
-            will_give_up = ','.join(map(str,will_give_up))
-        elif isinstance(will_give_up, int):
-            will_give_up = str(will_give_up)
+    def batch(self, offers, dry_run=False):
+        """Send offers to all other owners.
 
+        Args:
+            offers (list of dict): List of dicts containing offers to make.
+                Each offer must have the following fields:
+                    will_give_up (list of int): List of player ID's to trade away
+                    picks (list of dict): List of picks to trade for.
+                        Each pick must have the following fields:
+                            year (int)
+                            round (int)
+            dry_run (bool): If True, won't actually send offers, will only print out offers to be made.
+                Useful for double-checking that input parameters are correct.
+
+        Example:
+            >>> offers = [
+                {
+                    'will_give_up': [123, 456],
+                    'picks': [
+                        {
+                            'year': 2015,
+                            'round': 1
+                        }
+                    ]
+                },
+                {
+                    'will_give_up': [789],
+                    'picks': [
+                        {
+                            'year': 2015,
+                            'round': 1
+                        },
+                        {
+                            'year': 2016,
+                            'round': 2
+                        }
+                    ]
+                },
+            ]
+            >>> batch(offers)
+            This will send two offers to every other owner. One trade offering
+            players 123 and 456 in exchange for a 2015 1st, and a second trade
+            offering player 789 in exchange for a 2015 1st and a 2016 2nd
+        """
         req = 'http://football21.myfantasyleague.com/2014/export?TYPE=futureDraftPicks&L={}&W=&JSON=1'.format(self.league_id)
         resp = urllib2.urlopen(req)
         draft_picks = json.loads(resp.read())['futureDraftPicks']['franchise']
 
-        for draft_pick in draft_picks:
-            fid = draft_pick['id']
-            if fid != self.franchise_id:
-                picks = draft_pick['futureDraftPick']
+        for offer in offers:
+            will_give_up = ','.join(map(str,offer['will_give_up']))
+            # TODO: make this be able to handle multiple picks
+            pick_year = str(offer['picks'][0]['year'])
+            pick_round = str(offer['picks'][0]['round'])
+            for draft_pick in draft_picks:
+                fid = draft_pick['id']
+                if fid != self.franchise_id:
+                    picks = draft_pick['futureDraftPick']
 
-                # find first pick that satisfies predicate using 'next': http://stackoverflow.com/questions/8534256/find-first-element-in-a-sequence-that-matches-a-predicate
-                target_picks = [pick for pick in picks if pick['round'] == pick_round and pick['year'] == pick_year]
-                if target_picks:
-                    target_pick = target_picks[0]
-                    will_receive_id = 'FP_{0}_{1}_{2}'.format(
-                        target_pick['originalPickFor'],
-                        target_pick['year'],
-                        target_pick['round'])
-                    params = urllib.urlencode({
-                        'OFFEREDTO': fid,
-                        'WILL_GIVE_UP': will_give_up,
-                        'WILL_RECEIVE': will_receive_id,
-                        'TYPE': 'tradeProposal',
-                        'L': self.league_id})
-                    url = 'http://football19.myfantasyleague.com/2014/import?{}'.format(params)
-                    if not dry_run:
-                        self.opener.open(url)
-                    else:
-                        print('DRY RUN: {}'.format(url))
+                    # find first pick that satisfies predicate using 'next': http://stackoverflow.com/questions/8534256/find-first-element-in-a-sequence-that-matches-a-predicate
+                    target_picks = [pick for pick in picks if pick['round'] == pick_round and pick['year'] == pick_year]
+                    if target_picks:
+                        target_pick = target_picks[0]
+                        will_receive_id = 'FP_{0}_{1}_{2}'.format(
+                            target_pick['originalPickFor'],
+                            target_pick['year'],
+                            target_pick['round'])
+                        params = urllib.urlencode({
+                            'OFFEREDTO': fid,
+                            'WILL_GIVE_UP': will_give_up,
+                            'WILL_RECEIVE': will_receive_id,
+                            'TYPE': 'tradeProposal',
+                            'L': self.league_id})
+                        url = 'http://football19.myfantasyleague.com/2014/import?{}'.format(params)
+                        if not dry_run:
+                            self.opener.open(url)
+                        else:
+                            print('DRY RUN: {}'.format(url))
 
     def revoke_all(self, dry_run=False):
         req = 'http://football21.myfantasyleague.com/2014/export?TYPE=pendingTrades&L={}&JSON=1'.format(self.league_id)
@@ -93,7 +136,8 @@ if __name__ == "__main__":
         dry_run = sys.argv[2] == 'True'
     except IndexError:
         dry_run = True
-    t.batch([9823, 9448], '2015', '1', dry_run=dry_run)
+    offer = {'will_give_up': [9823, 9448], 'picks': [{'year': 2015, 'round': 1}]}
+    t.batch([offer],dry_run=dry_run)
 
 # WILL_GIVE_UP = player that user will give up; player that other owner will receive
 # WILL_RECEIVE = player that user will receive; player that other owner will give up
